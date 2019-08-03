fe49097a4b50b8a4ab00b6d04869a91f6d7f3f22
Aishu0708 <48148753+Aishu0708@users.noreply.github.com>
2019-08-03 18:35:45 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a: "))
+b=int(input("Enter b: "))
+cint(input("Enter c: "))
+if a>b and a>c:
+    print("a is larger")
+ elif:
+    print("b is larger")
+ else:
+    print("c is larger")
