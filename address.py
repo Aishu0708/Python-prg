e06f507d479eeb9ba07aa9d530dbf4818300dd08
Aishu0708 <48148753+Aishu0708@users.noreply.github.com>
2019-08-05 21:01:54 +0530

    Create address.py

---
+++ address.py
@@ -0,0 +1,6 @@
+def personal_details():
+    name=input("Enter name : ")
+    age=int(input("Enter age : "))
+    address = input("Enter address : ")
+    print("Name: {}\n Age: {}\n Address: {}".format(name, age, address))
+personal_details()
